7a66ee9e28585a1e74187d8124b6dbd2c164cbd2
vijay <vijaytechnical596@gmail.com>
2023-03-01 21:59:00 +0545

    updated for the app at 9:58pm

--- database/migrations/2023_03_01_004248_create_customers_table.php
+++ database/migrations/2023_03_01_004248_create_customers_table.php
@@ -15,10 +15,10 @@ return new class extends Migration
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->id('id');
-            $table->string('name',50);
-            $table->string('email',100);
-            $table->string('password');//255 characters
-            $table->enum('gender',["M","F","O"]);//predefined value
+            $table->string('name', 50);
+            $table->string('email', 100)->unique();
+            $table->string('password'); //255 characters
+            $table->enum('gender', ["M", "F", "O"])->default('O'); //predefined value
             $table->boolean('status')->default(1);
             $table->date('date_of_birth');
             $table->timestamps();
